71fbb11f087e9e54c4d6993e6d78b664577c1e1f
s2a0r0a4amra-blip <s2a0r0a4amra@gmail.com>
2025-10-19 14:53:02 +0100

    Update tp1.c

--- tp1.c
+++ tp1.c
@@ -1,2 +1,10 @@
-xcdcxcdcdcdcdcdc
-xddcdcdc
+#include<stdio.h>
+int main(){
+  int Mat[4][4],i,j,n;
+printf("Enter the elements of the MAT:/")
+  {
+for(i=0;i<4;i++)
+{ for(j=0;j<4;j++)
+  scanf("%d",&Mat[4][4]);
+}
+}
