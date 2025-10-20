cd310460fa84a23eb8f958b63e2f5d02416f8b43
s2a0r0a4amra-blip <s2a0r0a4amra@gmail.com>
2025-10-20 23:05:54 +0200

    Update tp1.c

--- tp1.c
+++ tp1.c
@@ -1,10 +1,49 @@
-#include<stdio.h>
-int main(){
-  int Mat[n][n],i,j,n;
-printf("Enter the elements of the MAT:/")
-  {
-    for(i=0;i<n;i++)
-          {   for(j=0;j<n;j++)
-  scanf("%d",&Mat[n][n]);
+
+#include <stdio.h>
+
+int main() {
+    int Mat[4][4];
+    int i, j, K;
+
+    printf("Donnez les elements de la matrice [4][4] :\n");
+    for(i = 0; i < 4; i++) {
+        for(j = 0; j < 4; j++) {
+            printf("Mat[%d][%d] = ", i, j);
+            scanf("%d", &Mat[i][j]);  
+        }
+    }
+
+    
+    printf("Matrice originale :\n");
+    for(i = 0; i < 4; i++) {
+        for(j = 0; j < 4; j++) {
+            printf("%3d ", Mat[i][j]);
+        }
+        printf("\n");
+    }
+
+
+    for(i = 0; i < 4; i++) {
+        for(j = i + 1; j < 4; j++) {
+           K = Mat[i][j];
+            Mat[i][j] = Mat[j][i];
+            Mat[j][i] =K;
+        }
+    }
+
+
+printf("Afficher le Mat[i][j]:\n");
+    for(i = 0; i < 4; i++) {
+        for(j = 0; j < 4 ; j++) {
+       
+            printf("%3d ", Mat[i][j]);
+      
+        }
+        printf("\n");
+    }
+
+    return 0;
 }
-}
+
+
+
